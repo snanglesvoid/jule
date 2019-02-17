32f7430d14091f7da18a7cff1ecf875ec0671c1f
Janik Hotz <snangles.void@gmail.com>
2019-02-17 22:10:04 +0100

    hallo jule

--- models/Startseite.js
+++ models/Startseite.js
@@ -5,7 +5,7 @@ const Startseite = new keystone.List('Startseite')
 
 Startseite.add({
     picture : { type: Types.CloudinaryImage },
-    text : { type: Types.Html, wysisyg: true, height: 300 },
+    text : { type: Types.Html, wysiwyg: true, height: 300 },
     active : { type: Types.Boolean }
 })
 
